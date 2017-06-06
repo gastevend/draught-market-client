353feeb527fd33395458c80f9e6205f724cc1fe3
gastevend <steven.duval@libertymutual.com>
2017-06-06 16:27:33 -0400

    This adds the save the button to the beer list and hides it
    Then when a user clicks on edit the edit and delete buttons hide and the
    save button appears. When the user fills out the info and then clicks
    save i am grabbing that information and storing it in a beer object. This
    is so I can eventually make the ajax request with this data

--- assets/scripts/beer/beer-events.js
+++ assets/scripts/beer/beer-events.js
@@ -13,19 +13,41 @@ const onDeleteBeer = function (event) {
     .catch(ui.deleteBeerFailure)
 }
 
-const addHandlers = function () {
+const onEditBeer = function () {
+  const id = $(event.target).attr('data-id')
+  $('.beer-editable[data-id=' + id + ']').attr('contenteditable', 'true')
+  console.log($('#beer-name').html(''))
+  $('#beer-name').focus()
+  $('.manage-pg-buttons[data-id=' + id + ']').toggle()
+  // $('.beer-editable[data-id=' + id + ']').replaceWith()
+}
+
+const onSaveBeer = function () {
+  const id = $(event.target).attr('data-id')
+  const beer = {}
+  beer.name = $('#beer-name[data-id=' + id + ']').html()
+  beer.price = $('#beer-price[data-id=' + id + ']').html()
+  console.log(beer)
+  // api.saveBeer(id)
+  //   .then(ui.saveBeerSuccess)
+  //   .catch(ui.saveBeerFailure)
+}
+
+const addBeerListHandlers = function () {
   $('.delete-button').on('click', onDeleteBeer)
+  $('.edit-button').on('click', onEditBeer)
+  $('.save-button').on('click', onSaveBeer)
 }
 
 const onGetBeers = function () {
   $('.beers-container').empty()
   api.getBeers()
     .then(ui.getBeersSuccess)
-    .then(addHandlers)
+    .then(addBeerListHandlers)
     .catch(ui.getBeersFailure)
 }
 
 module.exports = {
-  addHandlers,
+  addBeerListHandlers,
   onGetBeers
 }
--- assets/scripts/beer/ui.js
+++ assets/scripts/beer/ui.js
@@ -6,6 +6,7 @@ const getBeersSuccess = data => {
   console.log('getting beers back and they are ', data)
   const showBeersHtml = showBeers({ beers: data.beers })
   $('.beers-container').append(showBeersHtml)
+  $('.save-button').hide()
 }
 
 const getBeersFailure = error => {
